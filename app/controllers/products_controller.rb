a38d7fc08132f3cdedb722a25fb76ba1e92f4463
sasaki1125 <75967601+sasaki1125@users.noreply.github.com>
2021-01-19 12:09:03 +0900

    提出

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
 
   before_action :authenticate_user! , only: [:new]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @products = Product.includes(:user).order("created_at ASC")
@@ -36,8 +37,13 @@ class ProductsController < ApplicationController
 
 
   private
+
   def product_params
     params.require(:product).permit(:name,:price,:status_id,:category_id,:description,:judgment_id,:day_id,:prefecture_id,:image).merge(user_id: current_user.id)
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @product.user
   end
 
 
